bdd3010062d9d7ddc3a3c2000561847463f53954
David Gotz <gotz@unc.edu>
2026-03-13 18:49:08 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This simple script will give us a chance to learn how to use GitHub and its
+# associated tools like GitHub Classroom and GitHub CodeSpaces. This file is the
+# starting point for our work, and you will need to modify it to complete the 
+# assignment. You can edit this file directly in GitHub CodeSpaces, or you can
+# clone the repository to your local machine and edit it there. Once you have
+# made your changes, you will need to commit them and push them back to GitHub
+# so that they are saved and can be reviewed by your instructor.
+#
+# See the assignment instructions for more details about the edits you need to make 
+# to this file.
+#
+# This script does something very simple: it prints "Hello, World!" to the console.
+print("Hello, World!")
